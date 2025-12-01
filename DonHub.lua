3b4747d1a04a9ec213f7dcd018b6e21c155a13cd
don939716-gif <don939716@gmail.com>
2025-12-01 11:32:52 -0500

    Update DonHub.lua

--- DonHub.lua
+++ DonHub.lua
@@ -16,7 +16,7 @@ local Config = {
 }
 
 -- Animation Variables
-local WalkAnimID = "rbxassetid://1804263541"
+local WalkAnimID = "rbxassetid://180426354"
 local CurrentWalkTrack = nil
 
 --// UI SETUP \\--
