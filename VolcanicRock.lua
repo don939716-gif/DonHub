16896aa6403a0b790b91803e5ca6c4f4357ca19c
don939716-gif <don939716@gmail.com>
2025-12-06 18:27:58 -0500

    Update VolcanicRock.lua

--- VolcanicRock.lua
+++ VolcanicRock.lua
@@ -23,7 +23,7 @@ local LocalPlayer = Players.LocalPlayer
 local UserConfig = {
     Enabled = true,       
     HopDelay = 2,         -- Seconds to wait after 0 rocks found
-    StaleTimeout = 60,    -- Seconds to wait if rock count doesn't decrease
+    StaleTimeout = 120,    -- Seconds to wait if rock count doesn't decrease
     AutoReconnect = true, 
 }
 
